85bfeb2349b98ac1dae4f4662498ed284fdecdb0
romy <mama.marouainfo11@gmail.com>
2024-03-14 17:08:16 +0100

    danke sehr

---
+++ code.js
@@ -0,0 +1,33 @@
+//task1
+let y = 100 
+
+//void 
+function romy (x) {
+(x%2)==0 ? console.log("il est pair") : console.log("il est impair")
+}
+romy(y)
+
+let t=11
+const a = (b) =>{
+    return (b%2)==0 ? "il est pair" : "il est impair"
+    }
+console.log(a(t))
+
+//task2
+let tab = [1,2,3,4,5]
+let s = tab.join("")
+console.log(s)
+let somme =0 
+
+let stab =tab.map(x =>{
+    return somme += x}  )
+console.log(stab)
+console.log("la somme est le der elem :" +stab[4])
+
+
+
+console.log(s)
+
+
+
+console.log("halloo")
