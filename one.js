c14d3eb27b78bbc4c25d10cbcd96ae3dbae07b1a
romy <mama.marouainfo11@gmail.com>
2024-03-14 17:14:47 +0100

    einundzwei

---
+++ one.js
@@ -0,0 +1,14 @@
+//task1
+let y = 100 
+
+//void 
+function romy (x) {
+(x%2)==0 ? console.log("il est pair") : console.log("il est impair")
+}
+romy(y)
+
+let t=11
+const a = (b) =>{
+    return (b%2)==0 ? "il est pair" : "il est impair"
+    }
+console.log(a(t))
---
+++ two.js
@@ -0,0 +1,11 @@
+//task2
+let tab = [1,2,3,4,5]
+let s = tab.join("")
+console.log(s)
+let somme =0 
+
+let stab =tab.map(x =>{
+    return somme += x}  )
+console.log(stab)
+console.log("la somme est le der elem :" +stab[4])
+
